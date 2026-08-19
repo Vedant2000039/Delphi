a069817702ac13ba71a11d4c7ea64e9dc20a1480
VedantMulherkar <vmulherkar@xtsworld.in>
2026-08-19 16:29:05 +0530

    create MCP_Buyer_group

--- backend/apis/Onboarding/__init__.py
+++ backend/apis/Onboarding/__init__.py
@@ -1,9 +1,4 @@
 from fastapi import APIRouter
-
 from .enrichment import router as enrichment_router
-from .context_builder import router as context_builder_router
-
 router = APIRouter()
-
 router.include_router(enrichment_router)
-router.include_router(context_builder_router)
